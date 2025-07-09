3c5593c8567c279607e922c93d2536dc292b5ac8
Vishal Wadhera <30888002+wadherv@users.noreply.github.com>
2025-07-09 14:57:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "print_policy" {
+/*resource "null_resource" "print_policy" {
  provisioner "local-exec" {
     command = "Get-Content -Path '${path.module}\\policies\\instance-ssm-parameters-policy.json'"
     interpreter = ["PowerShell", "-Command"]
@@ -7,4 +7,10 @@ resource "null_resource" "print_policy" {
   triggers = {
     always_run = timestamp()
   }
+}
+*/
+
+output "policy_file_content" {
+  value = file("${path.module}/policies/instance-ssm-parameters-policy.json")
+  description = "Contents of the policy JSON file"
 }
